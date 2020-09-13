886a3785ed64264f6e2cf023c8261569968a6927
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-09-13 17:15:25 -0400

    Updating file.

---
+++ index.js
@@ -0,0 +1,25 @@
+// List of Questions
+let action = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'task',
+        choices: [
+            'View All Employees',
+            'View All Employees by Department',
+            'View All Employees by Manager',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+            'View All Roles',
+            'Add Role',
+            'Remove Role',
+            'View All Departments',
+            'View Budget by Department',
+            'Add Department',
+            'Remove Department',
+            'Exit Application'
+        ]
+    }
+];
